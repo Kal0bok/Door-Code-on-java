106752fd237c231d91b8ee096ec7edd06430652e
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-07 19:01:14 +0300

    Make new buttons, complete modal dialog and make size for window

--- src/doorcodee/doorcode.java
+++ src/doorcodee/doorcode.java
@@ -16,10 +16,12 @@ public class doorcode {
         return virkne;
     }
 		//dialog with buttons
-	private static String showGridChoice(String message, String title, String[] options) {
+	private static String showGridChoice(String message, String title) {
         final String[] selected = {null};
         
+        //make modal dialog
         JDialog dialog = new JDialog((Frame)null, title, true);
+        dialog.setLayout(new BorderLayout());
         
       //tittle 
         JTextArea textArea = new JTextArea(message);
@@ -29,8 +31,34 @@ public class doorcode {
         textArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         dialog.add(new JScrollPane(textArea), BorderLayout.NORTH);
         
-      
+      //panel with buttons 4x3
+        JPanel buttonPanel = new JPanel(new GridLayout(4, 3, 5, 5));
+        String[] numbers = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "", "Cancel",};
+        for(String number : numbers) {
+        	JButton button = new JButton(number);
+        	if(number.isEmpty()) {
+        		button.setEnabled(false);//empty cell no active
+        	}else if (number.equals("Cancel")) {
+        		button.addActionListener(e ->{
+        			selected[0] = null;// if press button cancel return null
+        			dialog.dispose();
+        		});
+        	
+        		}else {
+        			button.addActionListener(e ->{
+        				selected[0] = number;//save choosen number
+        				dialog.dispose();
+        			});
+        	}
+        	buttonPanel.add(button);
+        }
+        dialog.add(buttonPanel, BorderLayout.CENTER);
         
+        dialog.setSize(300, 400);//small size for door-code
+        dialog.setLocationRelativeTo(null); //make center on screen
+        dialog.setVisible(true);//show dialog
+        
+        return selected[0];//return choosen number or null
 	}
 	
 	public static void main(String[] args) {
