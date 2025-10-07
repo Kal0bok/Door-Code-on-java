661d95bc2cf880c172ed3100eac8b5ee9d6245d5
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-07 18:11:07 +0300

    Make imports

--- src/doorcodee/doorcode.java
+++ src/doorcodee/doorcode.java
@@ -1,10 +1,15 @@
 package doorcodee;
 
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class doorcode {
 
 	public static void main(String[] args) {
 
-		String a = "Hello";
+		
 
 	}
 
