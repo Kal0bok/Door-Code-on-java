898782a183899c34a283f54e592ade82cb5d60a9
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-07 19:09:24 +0300

    Make place for buttons

--- src/doorcodee/doorcode.java
+++ src/doorcodee/doorcode.java
@@ -4,14 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
-	//input field validation
 public class doorcode {
-	
-	String a = "I make it second time";
-	
 	public static void main(String[] args) {
 
+		// make window
+        JFrame frame = new JFrame("Door-code");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(4, 3, 5, 5)); // place for buttons 3x4 with spaces
+		
 	}
 
 }
