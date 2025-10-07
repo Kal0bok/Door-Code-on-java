beed49513582271819116bd06f1d3fcade389680
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-07 18:19:10 +0300

    Make input field validation

--- src/doorcodee/doorcode.java
+++ src/doorcodee/doorcode.java
@@ -5,11 +5,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+	//input field validation
 public class doorcode {
-
+	static String virkParb(String zinojums, String noklusejums) {
+        String virkne;
+        do {
+            virkne = JOptionPane.showInputDialog(zinojums, noklusejums);
+            if (virkne == null) return null;
+        } while (!virkne.matches("[a-zA-Z ]+"));
+        return virkne;
+    }
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-
-		
 
 	}
 
